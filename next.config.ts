5958d69dcf4f146af10dcf927e18e5a98270216a
jarydo <jarydnoahdiamond@gmail.com>
2025-08-15 22:07:21 -0700

    Add asset prefix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "https://showcase-demo-jaryd-diamonds-projects.vercel.app/"
+      : "",
 };
 
 export default nextConfig;
